c0ebcfb4bdac468233334f8eb9f29e70ea5fe755
Njiu <134121008+njiutheguru@users.noreply.github.com>
2024-07-10 22:25:32 +0300

    Update port in server.js

--- server/server.js
+++ server/server.js
@@ -28,7 +28,7 @@ module.exports = function runServer() {
   connectDB();
 
   // --------------------Run Server---------------------------
-  const port = process.env.PORT || 3000;
+  const port = process.env.PORT || 3001;
 
   app.listen(port, () => {
     console.log(`App running on :${port}`);
